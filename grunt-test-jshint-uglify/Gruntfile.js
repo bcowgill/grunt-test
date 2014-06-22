93ee77e2d54e9827fd2ff65382a451c95f11b516
Brent S.A. Cowgill <zardozcs@yahoo.com>
2014-06-22 23:07:11 +0100

    changes to some comments so that jshint doesn't consider them to be configuration options

--- grunt-test-jshint-uglify/Gruntfile.js
+++ grunt-test-jshint-uglify/Gruntfile.js
@@ -54,11 +54,11 @@ module.exports = function(grunt) {
         dest: 'dist/jasmine/jasmine-bundle.min.js'
       }
     },
-    // jshint warning codes are in:
+    // the jshint warning codes are in:
     // node_modules/grunt-contrib-jshint/node_modules/jshint/src/messages.js
     jshint: {
       options: {
-        //jshintrc: '.jshintrc',
+        //j s hintrc: '.jshintrc',
         //'indent': 3,
         'maxparams': 4,
         'maxdepth': 3,
