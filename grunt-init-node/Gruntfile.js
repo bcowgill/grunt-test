7899dc39f36557ee751da68889b5e1bfb22990da
Brent S.A. Cowgill <brent@blismedia.com>
2014-08-04 14:54:57 +0100

    Added jsdoc tags to Gruntfile.js for grunt-test/grunt-init-node

--- grunt-init-node/Gruntfile.js
+++ grunt-init-node/Gruntfile.js
@@ -1,5 +1,19 @@
-/*jshint indent: 2, maxstatements: 100 */
+/*jshint indent: 2, maxlen: 110, maxstatements: 100 */
 /*global module:false*/
+/**
+  @file Gruntfile.js
+  @author Brent S.A. Cowgill
+  @see {@link module:Gruntfile}
+  @description
+  Grunt build configuration.
+
+  @see {@link http://usejsdoc.org/ JSDoc Documentation}
+*/
+
+/**
+  Grunt build configuration.
+  @module Gruntfile
+*/
 module.exports = function(grunt) {
   'use strict';
 
@@ -8,6 +22,12 @@ module.exports = function(grunt) {
     nodeunit: {
       files: ['test/**/*_test.js'],
     },
+    /**
+      jshint validation of javascript code.
+      @see {@link https://github.com/gruntjs/grunt-contrib-jshint About jshint grunt plugin}
+      @see {@link http://jshint.com/docs/options/ jshint options}
+      @see {@link https://github.com/jshint/jshint/blob/master/src/messages.js Warning codes for jshint}
+    */
     jshint: {
       options: {
         jshintrc: '.jshintrc'
