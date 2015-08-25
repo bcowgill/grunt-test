33faa2be3f9a3d22867dfcb72b33822a80fe27e5
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-08-25 14:53:34 +0100

    make-mocha-dark.sh takes a parameter to specify which js file

--- grunt-test-jshint-uglify/test/mocha-chai/make-mocha-dark.sh
+++ grunt-test-jshint-uglify/test/mocha-chai/make-mocha-dark.sh
@@ -2,6 +2,6 @@
 # make-mocha-dark.sh > mocha-dark.js
 # grab the mocha.js file and change the canvas progress indicator to
 # a dark colour scheme.
-FILE=../../node_modules/mocha/mocha.js
+FILE=${1:-../../node_modules/mocha/mocha.js}
 
-perl -pne 's{fillText}{fillStyle = "yellow"; // BSAC DARK SCHEME\nctx.fillText}xmsg' $FILE
+perl -pne 's{fillText}{fillStyle = "yellow"; // BSAC DARK SCHEME\n    ctx.fillText}xmsg' $FILE
